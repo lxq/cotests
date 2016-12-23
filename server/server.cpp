21af78b57fd202a6145b5234b981e99c19c4c42c
lxq <lxq@weetgo.com>
2016-12-23 21:36:17 +0800

    init server.

--- server/server.cpp
+++ server/server.cpp
@@ -8,6 +8,18 @@ int main(int argc, char** argv)
 {
     co::init(argc, argv);
 
+    co::ConnectionDescriptionPtr connDesc = new co::ConnectionDescription;
+    connDesc->type = co::CONNECTIONTYPE_TCPIP;
+    connDesc->port = 12548;
+    connDesc->setHostname( "localhost" );
+
+    ServerPtr server = new Server();
+    server->addConnectionDescription(connDesc);
+
+    server->listen();
+    //co::NodePtr proxy
+
+    server = 0;
 
     co::exit();
 
