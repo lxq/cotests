8e98a6e8411101dd339c6f75b7971d5cd8676cb2
lxq <lxq@weetgo.com>
2016-12-30 21:24:25 +0800

    1.增加注释； 2.使用新的接口方法.

--- server/server.hpp
+++ server/server.hpp
@@ -50,27 +50,28 @@ private:
 
     bool _cmdSync( co::ICommand& command )
     {
-        std::cout<< "Received CMD_SYNC."<< std::endl;
-        ackRequest( command.getNode(), command.get< uint32_t >( ));
+        // NOTE: 如果Client没有发送Reuqest,调用ackRequest处理,则会导致异常崩溃.
+        // NOTE: 如果Client发送了Request,但没有调用ackRequest处理,则Client调用waitRequest会进阻塞等待.
+        ackRequest( command.getRemoteNode(), command.read< uint32_t >( ));
         return true;
     }
 
     bool _cmdData( co::ICommand& command )
     {
-        std::cout<< "Received CMD_DATA. contents: "<< std::endl;
-        command.getNode()->send( CMD_DATA_REPLY )
-            << command.get< uint32_t >() << ++_counter;
-        std::cout<< command.get<std::string>() <<std::endl;
+        std::cout<< "Received CMD_DATA. " << std::endl;
+        command.getRemoteNode()->send( CMD_DATA_REPLY )
+            << command.read< uint32_t >() << ++_counter;
+        std::cout<<"received contents: "<< command.read<std::string>() <<std::endl;
         return true;
     }
 
     bool _cmdDataReply( co::ICommand& command )
     {
-        std::cout<< "Received CMD_REPLY."<< std::endl;
-        const uint32_t request = command.get< uint32_t >();
-        const uint32_t result = command.get< uint32_t >();
-        //TEST( result == ++_counter );
+        std::cout<< "starting CMD_REPLY."<< std::endl;
+        const uint32_t request = command.read< uint32_t >();
+        const uint32_t result = command.read< uint32_t >();
         serveRequest( request, result );
+        std::cout<< "ending CMD_REPLY"<< std::endl;
         return true;
     }
 
